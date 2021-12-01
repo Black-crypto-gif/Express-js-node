06388318adba119194996758d98718208148745d
MELLAL Fethi <fethimellal@gmail.com>
2021-12-01 02:11:51 +0100

    express

--- 2-express-tutorial/app.js
+++ 2-express-tutorial/app.js
@@ -1,2 +1,30 @@
-const http = require('http')
+const express = require('express');
+const app = express();
 
+
+app.get('/', (req, res) => {
+    res.status(200).send('Hello World!');
+})
+app.get('/about', (req, res) => {
+    res.status(200).send('About Us');
+    console.log('About Us');
+})
+
+app.all('*',(req, res) => {
+    res.status(404).send('<h1>Page Not Found</h1>');
+})
+
+
+app.listen(8080,()=>{
+    console.log("server is running");
+})
+
+
+
+//app.get
+//app.post
+//app.put
+//app.delete
+//app.all
+//app.use
+//app.listen
